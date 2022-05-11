3476c0c561c5b60f38417d774c4e57fa9f3bb936
Hannah Kauber <hannah@Hannahs-MacBook-Pro.local>
2022-05-10 23:18:57 -0500

    pez.pl

---
+++ pez.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+@pez = ();
+
+print "pez is empty @pez\n";
+
+for(1..10){
+	print "Give me a flavor\n";
+chomp($foo=<>);
+push(@pez, $foo);
+}
+
+print "pez is full  @pez\n";
+for(1..10){
+
+}
+
+print"Now emptying pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+pop(@pez);
+print"@pez\n";
+
+print "Pez is now empty @pez\n";
+
