30b332cd3b6605c9ed7a1e26b8b62c3b1f879ebc
Hannah Kauber <hannah@Hannahs-MacBook-Pro.local>
2022-05-10 23:07:19 -0500

    calcs.pl

---
+++ calcs.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# Variable declaration 
+$foo = 5;
+$bar = 8; 
+$result; 
+
+#Doing math!
+#Addition
+$result = $foo + $bar; 
+print "$foo + $bar = $result\n"; 
+
+#Subtraction
+$result = $foo - $bar;
+print "$foo - $bar = $result\n";
+
+#Multipication
+$result = $foo * $bar; 
+print "$foo * $bar = $result\n";
+
+#Division
+$result = $foo / $bar; 
+print "$foo / $bar = $result\n";
+
+#Modulus
+$result = $foo % $bar; 
+print "$foo % $bar = $result\n"; 
+
